19bb5434e1bf37500381e2a3c9e8242569cd09b2
loveyoupeng <loveyoupeng@gmail.com>
2025-04-04 19:54:19 +0800

    perf python

--- tests/python/message_queu_test.py
+++ tests/python/message_queu_test.py
@@ -1,5 +1,5 @@
 from queue import Queue
-from time import sleep
+from time import sleep, perf_counter_ns
 
 import pytest
 from message_queue import MessageQueue  # type: ignore
@@ -7,16 +7,22 @@ from message_queue import MessageQueue  # type: ignore
 
 @pytest.mark.timeout(15)
 def test_message_queue():
-    result = Queue[int](1_500_000)
+    result = Queue[int](3_000_000)
 
     def process(value: int) -> None:
         result.put(value)
 
     queue = MessageQueue(callback=process, size=8)
     queue.start()
-    while result.qsize() < 1_000_000:
+    start = perf_counter_ns()
+    while result.qsize() < 2_000_000:
         sleep(0.001)
+    end = perf_counter_ns()
     del queue
+
+    print(
+        f"perf : {result.qsize()} / {float((end - start)) / 1_000_000_000} {float(end - start) / result.qsize()}"
+    )
 
     for i, v in enumerate(result.queue):
         assert i == v
